e1a9ad4b912f7a321ca073441646958b90c87f3d
Marco-Guarnera <Darkaquon93@gmail.com>
2024-09-23 16:32:43 +0200

    Added flags

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -18,6 +18,34 @@ export default {
             type: Number,
             required: true
         }
+    },
+    data: () => ({
+        imgName: ""
+    }),
+    computed: {
+        imgSrc() {
+            switch (this.originalLanguage) {
+                case "en":
+                    this.imgName = "en.png";
+                    break;
+                case "it":
+                    this.imgName = "it.png";
+                    break;
+                case "fr":
+                    this.imgName = "fr.png";
+                    break;
+                case "es":
+                    this.imgName = "es.png";
+                    break;
+                case "de":
+                    this.imgName = "de.png";
+                    break;
+                default:
+                    this.imgName = this.originalLanguage;
+                    break;
+            }
+            return new URL(`../assets/img/${this.imgName}`, import.meta.url).href;
+        }
     }
 }
 </script>
@@ -27,7 +55,8 @@ export default {
     <li class="list-item">
         <div v-text="title"></div>
         <div v-text="originalTitle"></div>
-        <div v-text="originalLanguage"></div>
+        <div v-if="imgName === originalLanguage" v-text="originalLanguage"></div>
+        <img v-else :src="imgSrc" :alt="title">
         <div v-text="voteAverage"></div>
     </li>
 </template>
